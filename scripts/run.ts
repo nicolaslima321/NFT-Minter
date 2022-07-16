e371806a0fd97ba92c8d75e0f1ec3cd5a4ed0d41
Nicolas Lima <nicolas.pedroso2145@gmail.com>
2022-07-16 01:30:01 -0300

    Implementing my own contract

---
+++ scripts/run.ts
@@ -0,0 +1,20 @@
+import { ethers } from "hardhat";
+
+const main = async () => {
+  const nftContractFactory = await ethers.getContractFactory("MyEpicNFT");
+  const nftContract = await nftContractFactory.deploy();
+  await nftContract.deployed();
+  console.log("Contrato implantado em:", nftContract.address);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
